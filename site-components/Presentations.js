aa615c224a77d3540b18e13c728d1d573e7b67f4
olafaloofian <bulluffalo@gmail.com>
2019-01-20 16:40:19 -0700

    Fix nav notification

--- site-components/Presentations.js
+++ site-components/Presentations.js
@@ -66,7 +66,7 @@ export default class Presentations extends React.Component {
                         return <S transition={transition} key={`slide-${i}`} />;
                         })}
                         </Deck>
-                        <div className={`notification-bar ${this.state.showNotification ? 'show-notification' : ''}`}>To navigate, use your arrow keys!!!</div>
+                        <div className={`notification-bar ${this.state.showNotification ? 'show-notification' : ''}`}>To navigate, use your arrow keys.</div>
                         <Link to='/'><svg xmlns="http://www.w3.org/2000/svg" className='icon-home' height={50} width={50} viewBox='0 0 24 24' style={{fill: theme.screen.colors.quaternary}}><path d="M20 7.093v-5.093h-3v2.093l3 3zm4 5.907l-12-12-12 12h3v10h7v-5h4v5h7v-10h3zm-5 8h-3v-5h-8v5h-3v-10.26l7-6.912 7 6.99v10.182z"/></svg>
                         </Link>
                     </React.Fragment>
